e98ee78447932653fab7a75315a68d654f64609b
Jaime Martinez <jemn1jemn@gmail.com>
2020-12-19 17:03:42 +0000

    adding push func

--- push_pall.c
+++ push_pall.c
@@ -1,6 +1,37 @@
 #include "monty.h"
 
 /**
+ * push - pushes an element to the stack.
+ * @head: pointer to the head of double l-list
+ * @line: content in file
+ * @line_num: line number
+ */
+
+ void push(stack_t **head, char *line, unsigned int line_num)
+{
+	char *first_n;
+	stack_t *node;
+
+	first_n = searchNumber(line);
+	if (!first_n)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_num);
+		free(line);
+		free(*head);
+		*head = NULL;
+		exit(EXIT_FAILURE);
+	}
+	node = add_node(head, atoi(first_n));
+	if (!node)
+	{
+		puts("Error: malloc failed");
+		free_stk(*head);
+		exit(EXIT_FAILURE);
+	}
+	free(line);
+}
+
+/**
  * pall - prints all elents in a stack from top
  * @head: head of the stack
  * @line_num: number of lines

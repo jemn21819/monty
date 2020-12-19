338f072dfd16395444792fa5771ff8167dcedbe5
Jaime Martinez <jemn1jemn@gmail.com>
2020-12-19 18:40:32 +0000

    adding push func

--- string.c
+++ string.c
@@ -43,17 +43,20 @@ int _strcmp(char *s1, char *s2)
  */
 int _strncmp(char *s1, char *s2, int n)
 {
-	while (n > 0)
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
 	{
-		if (*s1 != *s2)
-		{
-			return (*s1 - *s2);
-		}
-		++s1;
-		++s2;
-		n--;
+		if (s1[i] == s2[i])
+			++i;
 	}
-	return (0);
+	else
+	{
+		return (s1[i] - s2[i])
+	}
+	if (i == n && s1[i] == ' ' || s1[i] == '\t' || s1[i] == '\0' || s1[i] == '\n')
+		return (0);
+	return (EXIT_FAILURE);
 }
 
 /**
